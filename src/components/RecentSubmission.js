42226931190c38d5acf96693c4592603c8dd2254
jackie <jacquelyn.cheng@me.com>
2018-12-11 15:55:27 -0800

    added lastline to recent submission, also proptypes

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './RecentSubmission.css';
+import PropTypes from 'prop-types';
 
 const RecentSubmission = (props) => {
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{ props.lastLine }</p>
     </div>
   );
 }
+
+RecentSubmission.propTypes = {
+  isSubmitted: PropTypes.bool.isRequired,
+  lastLine: PropTypes.string.isRequired,
+};
 
 export default RecentSubmission;
